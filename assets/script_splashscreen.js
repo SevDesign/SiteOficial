2a336ad9e1167577d225bc8ac2c42674eb71cede
PC Uninta - tarde <caiomm076@gmail.com>
2026-08-28 15:43:33 -0300

    fix link

--- assets/script_splashscreen.js
+++ assets/script_splashscreen.js
@@ -2,16 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const splash = document.getElementById('splashScreen');
     
     if (splash) {
-        document.body.style.overflow = 'hidden';
 
         setTimeout(() => {
             splash.classList.add('fade-out');
-            document.body.style.overflow = 'auto';
 
             setTimeout(() => {
                 splash.remove();
-            }, 800);
+            }, 700);
             
-        }, 2000);
+        }, 1000);
     }
 });
